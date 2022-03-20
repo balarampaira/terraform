d27295debeb31a6b7f99c5b8674277daa4b5839f
balarampaira <balaram.pairagmail.com>
2022-03-20 16:28:11 +0530

    updated files

---
+++ data_source.tf
@@ -0,0 +1,14 @@
+data "aws_ami" "amazon_linux" {
+    owners      = ["amazon"]
+    most_recent = true
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+
+    }
+    filter {
+        name   = "state"
+        values = ["available"]
+        }
+  
+}
